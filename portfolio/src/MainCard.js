108be2edbc16065334c20fe98a39887ed689769d
Ritwik Goel <ritwikgoel@yahoo.com>
2024-01-16 22:41:22 -0500

    checkpoint

--- portfolio/src/MainCard.js
+++ portfolio/src/MainCard.js
@@ -1,15 +1,36 @@
-// MainCard.jsx
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Navigator from './Navigator';
 
 function MainCard() {
+  const audioRef = useRef(null);
+  const [isMusicPlaying, setMusicPlaying] = useState(false);
+
+  const toggleMusic = () => {
+    const audioElement = audioRef.current;
+
+    if (audioElement) {
+      if (isMusicPlaying) {
+        audioElement.pause();
+      } else {
+        audioElement.play();
+      }
+      setMusicPlaying(!isMusicPlaying);
+    }
+  };
+
   return (
     <div className="maincard">
-      
       <h1>Ritwik Goel</h1>
-      
       <h3>Student @ Columbia University</h3>
-      <h5>I build user-friendly software with a keen eye for detail, ensuring a <br></br><br></br>seamless experience across different platforms and applications.</h5>
+      <h5>
+        I build user-friendly software with a keen eye for detail, ensuring a
+        <br />
+        seamless experience across different platforms and applications.
+      </h5>
+      <button onClick={toggleMusic}>
+        {isMusicPlaying ? '🔇' : '   🎵   '}
+      </button>
+      <audio ref={audioRef} src="/bg.mp3" loop />
       <Navigator />
     </div>
   );
