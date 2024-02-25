fc87ced0bd53ad37fb8164b03d62bb860faf095f
Ritwik Goel <ritwikgoel@yahoo.com>
2024-02-24 19:36:39 -0500

    UI FIxes

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
       <section id="about" />
 
       <div className="about">
-      Welcome to my digital realm! I'm Ritwik Goel, studying Computer Engineering at Columbia University. My passion lies at the crossroads of technology and innovation, with a particular focus on cloud computing, analytics, and software development. My portfolio showcases my hands-on experience and curiosity in using technology to drive meaningful solutions. Let's explore the ever-changing digital landscape together, where I combine creativity and technical skills to make a difference.
+      Welcome to my digital realm! I'm Ritwik Goel, studying Computer Engineering at Columbia University. My passion lies at the crossroads of technology and innovation, with a particular focus on cloud computing, analytics, and software development. My Github showcases my hands-on experience and curiosity in using technology to drive meaningful solutions. Let's explore the ever-changing digital landscape together, where I combine creativity and technical skills to make a difference.
       </div>
     </div>
   );
